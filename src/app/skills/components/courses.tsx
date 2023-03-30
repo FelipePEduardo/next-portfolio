fd7661a4d63c3810424172343a29f3f24c74831e
Felipe Eduardo <Felipeeduardol7@outlook.com>
2023-03-30 15:55:52 -0300

    :pencil: style: adjusted explorer text

--- src/app/skills/components/courses.tsx
+++ src/app/skills/components/courses.tsx
@@ -6,18 +6,52 @@ export function Courses() {
         <span className="italic underline text-lg">Ignite - Rocketseat</span>
         <p className='bg-purpleDark-700 p-5 rounded-xl mt-2 '> 
           Programa focado na especialização de stacks como ReactJS utilizando
-          ferramentas como Next.js, TypeScript, Context Api, React Hooks,
-          Styled-Components, Stitches e outras coisas.
+          ferramentas como <strong>Next.js</strong>, <strong>TypeScript</strong>, <strong>Context Api</strong>, <strong>React Hooks</strong>, <strong>Styled-Components</strong>, <strong>Stitches</strong> e outras coisas.
         </p>
       </div>
 
       <div className='flex flex-col items-center'>
         <span className="italic underline text-lg">Explorer - Rocketseat</span>
-        <p className='bg-purpleDark-700 p-5 rounded-xl mt-2'> 
-          Programa focado na especialização de stacks como ReactJS utilizando
-          ferramentas como Next.js, TypeScript, Context Api, React Hooks,
-          Styled-Components, Stitches e outras coisas.
+        <p className='bg-purpleDark-700 p-5 rounded-xl mt-2 flex flex-col gap-2'> 
+        Programa da Rocketseat focado em formar e desenvolver habilidades técnicas e comportamentais com foco na primeira oportunidade como programador.
+
+        <strong>
+          Conteúdos técnicos abordados:
+        </strong>
+        
+        <span>
+          - HTML e CSS
+        </span>
+
+        <span>
+          - JavaScript
+        </span>
+
+        <span>
+          - Git e GitHub
+        </span>
+        
+        <span>
+          - Node.js
+        </span>
+        
+        <span>
+          - ReactJS
+        </span>
+        
+        <span>
+          - APIs
+        </span>
+        
+        <span>
+          - Deploy
+        </span>
+        
+        <span>
+          - Desafio final (Desenvolvimento de um Cardápio Digital utilizando os conceitos técnicos abordados no decorrer da formação).
+        </span>
         </p>
+
       </div>
 
     </div> 
