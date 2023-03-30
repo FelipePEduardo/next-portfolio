e51f639a4f6e1ef7e936a3f71b0b39029a21ed71
Felipe Eduardo <Felipeeduardol7@outlook.com>
2023-03-30 15:42:43 -0300

    :lipstick: feat: adjusted header font

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,7 @@ export function Header() {
   return (
     <header className="w-full fixed top-0 left-0 px-8 py-4 backdrop-blur saturate-150 flex items-center">
       <div className="w-full max-w-7xl mx-auto flex items-center justify-between">
-        <Link href="/" className="text-zinc-100 text-4xl font-bold">&lt;Portfólio &frasl;&gt;</Link>
+        <Link href="/" className="text-zinc-100 text-3xl md:text-4xl font-bold">&lt;Portfólio &frasl;&gt;</Link>
 
         <nav className="hidden md:block">
           <ul className="flex items-center gap-4 text-sm">
